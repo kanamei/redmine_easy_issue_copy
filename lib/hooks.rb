8d887171f28fab1a4b73466c4ffd9758b83d3988
shim0mura <tatshimomura@gmail.com>
2014-07-15 13:29:44 +0900

    コピーボタンを押した時に新規チケット作成画面に飛ぶよう変更

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -2,7 +2,7 @@ module Redmine_easy_issue_copy
   class Hooks < Redmine::Hook::ViewListener
     def view_issues_show_details_bottom(context)
       context[:controller].send(:render_to_string, {
-        :partial => 'easy_issue_copy/form',
+        :partial => 'easy_issue_copy/show_link',
         :locals => context
       })
     end
@@ -11,24 +11,29 @@ module Redmine_easy_issue_copy
       if context[:params][:issue][:easy_issue_copy]
         issue = context[:issue]
         EasyIssueCopyIssueRelation.create!(
-          original_issue_id: context[:params][:issue][:easy_issue_copy][:parent_id].to_i,
+          original_issue_id: context[:params][:issue][:easy_issue_copy][:original_issue_id].to_i,
           copied_issue_id: issue.id.to_i
         )
       end
     end
-  
-    def controller_issues_new_before_save(context)
-      if context[:params][:issue][:easy_issue_copy]
-        issue = Issue.find(context[:params][:issue][:easy_issue_copy][:original_issue_id])
-        original_watcher_user_ids = issue.watchers ? issue.watchers.map{|watcher| watcher.user_id} : []
-        project_member_ids = context[:project].members.map{|member| member.user_id}
-        inherit_watcher_user_ids = original_watcher_user_ids.select do |user_id|
-          project_member_ids.include?(user_id)
-        end
-        User.where(id: inherit_watcher_user_ids).each do |user|
-          context[:issue].add_watcher(user)
-        end
-      end 
+
+    def view_issues_new_top(context)
+      if context[:request].params.has_key?("easy_issue_copy")
+        context[:controller].send(:render_to_string, {
+          :partial => 'easy_issue_copy/copy_header',
+          :locals => context
+        })
+      end
     end
+
+    def view_issues_form_details_bottom(context)
+      if context[:request].params.has_key?("easy_issue_copy")
+        context[:controller].send(:render_to_string, {
+          :partial => 'easy_issue_copy/form',
+          :locals => context
+        })
+      end
+    end
+
   end
 end
